aac61747a87487e5ac9ce003f56f7c0b1971eb0a
BenjaminOrtizJr <bortizjr84@outlook.com>
2022-10-26 11:55:48 -0400

    #

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -8,6 +8,15 @@ const Header = () => {
           <div className="header__image-container">
               <img src={HeaderImage} alt="image of workers working with files" />
           </div>
+          <div className="header__text-container">
+              <h2>All your files in one secure location, accessible anywhere.</h2>
+              <p>Fylo stores your most important files in one secure location. 
+                 Access them wherever you need, share and collaborate with friends, 
+                 family, and co-workers.
+              </p>
+              <input type="text" placeholder="Enter your email..." />
+              <button>Get Started</button>
+            </div>
       </header>
   )
 }
